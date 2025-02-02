175efc1ae5038ffb6d342bbb082db343df514ac9
Sidinei P. Gonchoroski <sidinei.pereira@gmail.com>
2025-02-02 18:17:02 -0300

    feat: initial config

---
+++ src/test/java/br/com/spg/bancodigital/BancodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spg.bancodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancodigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
